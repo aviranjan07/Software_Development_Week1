f1270e8bb1d2896f828b58b6fc87ceccb9a5d1da
Aviranjan <aviranjand@gmail.com>
2026-03-10 00:13:55 +0530

    Add two Number

---
+++ Week1.java
@@ -0,0 +1,29 @@
+/* public class Week1 {
+    public static void main(String[] args) {
+        System.out.println("Week 1 - Software Development Journey");
+    }
+}
+ */
+
+    // Print Hello World
+ /* public class Week1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}  */
+
+      // Add Two Numbers
+import java.util.Scanner;      
+public class Week1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Two Number");
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int sum = n1 + n2;
+        
+        System.out.println("Sum is: " + sum);
+    }
+}  
