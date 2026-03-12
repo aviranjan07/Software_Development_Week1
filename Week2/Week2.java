30543297f62037d695466545fea6305b63b65068
Aviranjan <ad3447578@gmail.com>
2026-03-12 23:28:29 +0530

    AND Operator (&&)

--- Week2/Week2.java
+++ Week2/Week2.java
@@ -11,4 +11,16 @@
     }
 } */
 
-    
+// AND Operator (&&)
+/* public class Week2 {
+
+    public static void main(String[] args) {
+        int age = 17;
+        if (age > 18 && age < 60) {
+            System.out.println("Eligible for job");
+        } else {
+            System.out.println("Rejected");
+        }
+    }
+}
+ */
