d388029508abcb7168730bbe089114af7e77d75f
Aviranjan <ad3447578@gmail.com>
2026-03-15 23:13:23 +0530

    Square Pattern

--- Week2/Week2.java
+++ Week2/Week2.java
@@ -120,3 +120,23 @@
         }
     }
 } */
+
+    // Square Pattern
+/* *****
+*****
+*****
+*****
+***** */
+/* public class Week2 {
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} */
+
+    
