97b264382a32f42ee9abde3a91865d297da8b3d6
Aviranjan <ad3447578@gmail.com>
2026-03-15 23:17:31 +0530

    Right Triangle

--- Week2/Week2.java
+++ Week2/Week2.java
@@ -121,7 +121,7 @@
     }
 } */
 
-    // Square Pattern
+// Square Pattern
 /* *****
 *****
 *****
@@ -139,4 +139,21 @@
     }
 } */
 
-    
+// Right Triangle
+/* *
+**
+***
+****
+***** */
+
+/*public class Week2 {
+ 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+} */
