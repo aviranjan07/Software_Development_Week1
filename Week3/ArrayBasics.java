cf317558a8941490b69ee5cafea3e741a30401ea
Aviranjan <ad3447578@gmail.com>
2026-03-21 09:40:31 +0530

    basics array

---
+++ Week3/ArrayBasics.java
@@ -0,0 +1,7 @@
+public class ArrayBasics {
+    public static void main(String[] args) {
+      int[] number = {10, 20, 30, 40, 50};
+      int value = number[0];
+      System.out.println(value);
+    }
+}
