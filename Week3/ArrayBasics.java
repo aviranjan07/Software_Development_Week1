ad162554bfb532a79a81ba0f693212be82ba63f4
Aviranjan <ad3447578@gmail.com>
2026-03-21 10:22:25 +0530

    store String on Array

--- Week3/ArrayBasics.java
+++ Week3/ArrayBasics.java
@@ -1,7 +1,16 @@
-public class ArrayBasics {
+/* public class ArrayBasics {
     public static void main(String[] args) {
       int[] number = {10, 20, 30, 40, 50};
       int value = number[0];
       System.out.println(value);
+    }
+}
+ */
+  //Store String 
+ public class ArrayBasics {
+
+    public static void main(String[] args) {
+        String[] name = {"Aviranjan", "Rahul", "Ashim"};
+        System.out.println(name[2]);
     }
 }
