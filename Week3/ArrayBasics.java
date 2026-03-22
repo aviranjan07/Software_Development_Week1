dcde59fd560c783459cea43fb1a70851f96e023e
Aviranjan <ad3447578@gmail.com>
2026-03-22 08:52:19 +0530

    Array Length

--- Week3/ArrayBasics.java
+++ Week3/ArrayBasics.java
@@ -33,3 +33,12 @@
     }
         
     } */
+//    Array length
+/* public class ArrayBasics {
+
+    public static void main(String[] args) {
+        int[] number = {10, 20, 30, 40, 50};
+        System.out.println(number.length);
+
+    }
+} */
