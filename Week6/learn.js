eff14c3cc595151e9fd9bbd97313612636b784e7
Aviranjan <ad3447578@gmail.com>
2026-04-11 23:06:15 +0530

    check Type of value

--- Week6/learn.js
+++ Week6/learn.js
@@ -3,6 +3,6 @@ let userName = "aviranjan";
 let age = 25;
 let isStudent = true;
 
-console.log(userName);
-console.log(age);
-console.log(isStudent);
+console.log(typeof userName);
+console.log(typeof age);
+console.log(typeof isStudent);
